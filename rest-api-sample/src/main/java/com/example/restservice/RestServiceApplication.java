a54c02807362aded5b6eeae1bbb8b789a69cd057
yamadaitsuki <yamada351035@gmail.com>
2022-02-20 17:55:17 +0900

    finish rest-api-sample

--- rest-api-sample/src/main/java/com/example/restservice/RestServiceApplication.java
+++ rest-api-sample/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,11 @@
-package com.example.restapisample;
+package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiSampleApplication {
-
+public class RestServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RestApiSampleApplication.class, args);
+        SpringApplication.run(RestServiceApplication.class, args);
     }
-
 }
